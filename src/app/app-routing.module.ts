10be7187a485fff44493549012a55e09c7f667d9
vcte <vcte500@gmail.com>
2018-08-18 23:26:59 -0400

    agrgamos navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {AdminComponent} from './components/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HomeComponent},
+  { path: 'navbar' , component: NavbarComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
